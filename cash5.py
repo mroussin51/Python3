d910842a566956f070ac74904a760bb9aad33906
mroussin51 <121131185+mroussin51@users.noreply.github.com>
2022-12-20 15:50:16 -0500

    Add files via upload

---
+++ cash5.py
@@ -0,0 +1,28 @@
+import random
+number1 = random.randint(1, 5)
+print(number1)
+number2 = random.randint(1, 5)
+if number2 != number1 and number2 == number1:
+    print(number2)
+#if number2 == number1:
+#    number2 = random.randint(1, 5)
+#   number2 = random.randint(1, 5)
+#print(number2) 
+
+#number3 = random.randint(1, 5)
+#if number3 != number1 and number2:
+#    print(number3)
+#     else number3 = random.randint(1, 5)
+#number3 = random.randint(1, 5)
+# print(number3)
+#if number3 != number1 or number2:
+#   number3 = random.randint(1, 5)
+#print(number3)
+# print(number3)
+#number4 = random.randint(1, 43)
+#if number4 == number1 == number2 == number3:
+#	print(random.randint(1, 43))
+# print(number4)
+#number5 = random.randint(1, 43)
+#if number5 == number1 == number2 == number3 == number4:
+#    print(random.randint(1, 43))
